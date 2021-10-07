6dc4beabf7b4f5474ee0ee99bd727b48af4c4494
Arun Jangir <jangirarun786@gmail.com>
2021-10-08 00:51:31 +0300

    added QuickFindUF.java

---
+++ 001-union-find/QuickFindUF.java
@@ -0,0 +1,13 @@
+/*
+Two element P,Q are connected if they have same id.
+Let's implemet it
+*/
+public class QuickFindUF{
+private int[] id;
+public QuickFindUF(int N){
+id = new int[N];
+for (int i =0; i < N; i++){
+    id[i]=i;
+}
+}
+}
