fe15ee622069589e2ed574375ecf5bf64c207f17
Arun Jangir <jangirarun786@gmail.com>
2021-10-08 00:17:26 +0300

    Added comments

--- 001-union-find/UF.java
+++ 001-union-find/UF.java
@@ -1,3 +1,8 @@
+/*
+module load java
+Compile by : javac UF.java
+run     by : java UF
+ */
 public class UF{
     public static void main(String[] args){
         System.out.println("Hello World");
