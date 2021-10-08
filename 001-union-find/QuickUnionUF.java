75418272077c7e7be3a1421ad579995dbf22f88d
arunjangir <jangirarun786@gmail.com>
2021-10-08 14:49:29 +0300

    Implementation of path compression added

--- 001-union-find/QuickUnionUF.java
+++ 001-union-find/QuickUnionUF.java
@@ -14,6 +14,7 @@ public class QuickUnionUF {
 
     private int root(int i) {
         while (i != id[i]) {
+            id[i] = id[id[i]]    //Path Compression
             i = id[i];
         }
         return i;
