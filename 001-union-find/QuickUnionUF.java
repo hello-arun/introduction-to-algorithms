53f1ae6f95d7967c8ff9d3fd07c3d56aa5433e94
arunjangir <jangirarun786@gmail.com>
2021-10-12 02:10:34 +0300

    Path compression added

--- 001-union-find/QuickUnionUF.java
+++ 001-union-find/QuickUnionUF.java
@@ -3,18 +3,20 @@ public class QuickUnionUF {
      * id contains root information of each number initially id of each number is
      * number itself
      */
-    private int[] id;
+    private int[] id,sz;
 
     public QuickUnionUF(int N) {
         id = new int[N];
+        sz = new int[N];
         for (int i = 0; i < id.length; i++) {
             id[i] = i;
+            sz[i] = 1;
         }
     }
 
     private int root(int i) {
         while (i != id[i]) {
-            id[i] = id[id[i]];    //Path Compression
+            id[i] = id[id[i]]; // Path Compression
             i = id[i];
         }
         return i;
@@ -30,6 +32,14 @@ public class QuickUnionUF {
          */
         int i = root(p);
         int j = root(q);
-        id[i] = j;
+        if (i == j)
+            return;
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
     }
 }
